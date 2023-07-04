a988a7112a5dbae25495c4a29f996012b316ad15
Poostoi <oleg.nour@bk.ru>
2023-07-05 00:05:10 +0300

    feat: ovveraid work bot

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import update as update
-from telegram.ext import Updater, CommandHandler
-import requests
-import re
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('6369659318:AAGFf7v8U_ajecx17FS_5itGE16NP1HGnts')
 
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()
-    return contents['url']
+@bot.message_handler(commands=['start'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text='Привет', url='https://habr.com/ru/all/')
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, 'asdfsf', reply_markup=markup)
 
 
-def send_messege(bot, update):
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=get_url())
-
-
-def main():
-    updater = Updater('6369659318:AAGFf7v8U_ajecx17FS_5itGE16NP1HGnts')
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('send_message', send_messege))
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+bot.polling(none_stop=True, interval=0)
